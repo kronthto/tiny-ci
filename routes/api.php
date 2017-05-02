aac5b89e3fe90eaddf994fcfc55d0a4c7ee2057a
Tobias Kronthaler <kronthto@gmail.com>
2017-05-02 22:46:04 +0200

    Fixed hook method

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@
 |
 */
 
-Route::middleware('xhubsig')->get('/hook/{slug}', 'HookController@takePayloadAction');
+Route::middleware('xhubsig')->post('/hook/{slug}', 'HookController@takePayloadAction');
